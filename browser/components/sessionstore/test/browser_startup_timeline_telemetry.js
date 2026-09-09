c6220edc63165e9753383b9d99b700f482fbc704
Huansock <Huan.sock@gmail.com>
2026-09-09 14:36:05 +0200

    Delete browser directory
    Signed-off-by: Huansock <Huan.sock@gmail.com>

--- browser/components/sessionstore/test/browser_startup_timeline_telemetry.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-add_task(async function sessionRestoreInitialized() {
-  Assert.greater(
-    Glean.sessionRestore.startupTimeline.sessionRestoreInitialized.testGetValue(),
-    0,
-    "SessionStore.init() recorded a startup timeline value."
-  );
-});
